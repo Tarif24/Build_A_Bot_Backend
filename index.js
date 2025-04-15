dd6115c078048c7ec94534dab52d7045933db7a7
Tarif24 <tarif24@hotmail.com>
2025-04-15 13:20:08 -0400

    created a simple server using express and added cors and bodyparser as middleware

---
+++ index.js
@@ -0,0 +1,21 @@
+import bodyParser from "body-parser";
+import express from "express";
+import dotenv from "dotenv";
+import route from "./routes/routes.js";
+import { createRequire } from "module";
+
+// Init and config seting up express app and dotenc
+const require = createRequire(import.meta.url);
+const app = express();
+const cors = require("cors");
+dotenv.config();
+
+// Setting up express app and middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
+});
---
+++ routes/routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+//import {} from "../controller/jobController.js";
+
+const route = express.Router();
+
+//route.post("/query", );
+
+export default route;
